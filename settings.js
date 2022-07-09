1fa3bf378259d3b08f878adb1a2fa9310b092146
Syenzkyy <107863901+Syenzkyy@users.noreply.github.com>
2022-07-09 09:38:33 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['60179630068'] //ur owner number
+global.ownername = "Syenzky Md" //ur owner name
+global.ytname = "YT: Akiravfx._" //ur yt chanel name
+global.socialm = "GitHub: Syenzkyy" //ur github or insta name
+global.location = "Malaysia, Sabah, Tuaran" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['60179630068']
+global.ownertag = '60179630068' //ur tag number
+global.botname = 'Syenzky Bot MD V5' //ur bot name
+global.linkz = "https://youtube.com/channel/UCQKyzmfPBlSKmfDCjV04xWw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCQKyzmfPBlSKmfDCjV04xWw" //ur website to be displayed
+global.botscript = 'https://github.com/Syenzkyy/YenzzBotMdUpdate5.0' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "Syenzky Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "🦄 Syenzky Dream Outhor" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['60179630068'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
